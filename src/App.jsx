53e0c62d2e3a6fdb726f8a2f9be329dc2533fa33
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-14 17:11:52 +0530

    added React Router Dom and initialised pagees

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route>
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
