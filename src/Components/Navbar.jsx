91c1e69c49387cb2c3a6a94014e0aa03113c822e
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-14 17:31:51 +0530

    initial styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div style={{display:"flex",alignItems:"space-between"}}>
       <span className='Logo'>Redux Store</span>
       <div>
         <Link className="navLink" to="/">Home</Link>
