c991f68ca616a4328f7e7a832cfddf48647e800e
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-17 16:54:29 +0530

    fixed naavbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div style={{display:"flex",alignItems:"space-between"}}>
+    <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
       <span className='Logo'>Redux Store</span>
       <div>
         <Link className="navLink" to="/">Home</Link>
