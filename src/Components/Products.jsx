a821b66adf53aafa3689858e146c09590031bc18
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-17 16:41:46 +0530

    fetched data from an API

---
+++ src/Components/Products.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const Products = () => {
+
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchProducts = async () =>{
+            const res = await fetch('https://fakestoreapi.com/products')
+            const data = await res.json()
+            console.log(data)
+            setProducts(data)
+        }
+        fetchProducts()
+    },[])
+
+  return (
+    <div className='productsWrapper'>
+        {
+            products.map((product) => (
+                <div className='card' key={product.id}>
+                    <img src={product.image} alt="" />
+                    <h4>{product.title}</h4>
+                    <h5>{product.price}</h5>
+                    <button className='btn'>add to Cart</button>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Products
