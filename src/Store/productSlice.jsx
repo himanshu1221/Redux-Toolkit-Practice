73447ed3e5a9432cd7c0f84eff1007fa527e0b86
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-20 15:11:33 +0530

    setting slice for products

---
+++ src/Store/productSlice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const STATUSES = Object.freeze({
+    IDLE:'idle',
+    ERROR:'error',
+    LOADING:'loading',
+})
+
+const productSlice = createSlice({
+    name:'Products',
+    initialState:{
+        data:[],
+        status:STATUSES.IDLE,
+    },
+    reducers:{
+        setProducts(state,action){
+            state.data = action.payload
+        },
+    },
+});
+
+export const { add , remove } = productSlice.actions
+
+export default productSlice.reducer
