eb34d1aff548fdfc4fadc5785f9b04b7930adf76
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-20 14:48:10 +0530

    items are succefully deleted from cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { remove } from '../Store/cartSlice';
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state)=>state.cart)
+  const handleDelete = (productId) => {
+      dispatch(remove(productId))
+  }
   return (
-    <div>Cart</div>
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {
+          products.map(product => (
+            <div className="cartCard" key={product.id}>
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+              <h5>{product.price}</h5>
+              <button onClick={()=>handleDelete(product.id)} className='btn'>Remove</button>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
